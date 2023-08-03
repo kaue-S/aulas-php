d45d3c7561c0c9a746d6189147d80112a35c28c0
Kauê Evangelista <kauesilva38@hotmail.com>
2023-08-03 09:37:08 -0300

    Update funcoes.php

--- exemplos/funcoes.php
+++ exemplos/funcoes.php
@@ -41,5 +41,48 @@
 
     <p>estamos no curso de <?=dadosCurso()?></p>
     <p><?=dadosCurso()?>é ministrado no Senac Penha.</p>
+    <hr>
+
+    <h2>Função com parâmetros (ou argumentos)</h2>
+
+    <?php
+        function soma( $valor1, $valor2, $valor3 ){
+            //variável de escopo local
+            $total = $valor1 + $valor2 + $valor3;
+            return $total;
+        }
+    ?>
+    <!-- chamando e exibindo retorno -->
+    <p>Resultado 1: <?=soma(10, 20, 5)?></p>
+    <p>Resultado 2: <?=soma(23, 84, 1000)?></p>
+
+    <?php
+    //chamando e aguardando o retorno
+        $resultado3 = soma(500, 480, 700);
+    ?>
+
+    <p>Resultado 3: <?=$resultado3?></p>
+    
+    <?php
+        if($resultado3 > 100) { ?>
+        <p>Vinicius foi reprovado</p>
+    <?php }?>
+
+    <?php
+        if( soma(2, 3, 10) >= 10){ ?>
+            <p>Kauê reprovado!</p>
+    <?php } ?>
+
+            
+    <h3>Função com parâmetros opcionais</h3>
+    <?php
+    //$pessoa é um parâmetro opcional com um valor padrão 
+        function saudacao($mensagem, $pessoa = "fulano(a)"){
+            return "Olá, $mensagem $pessoa!";
+        }
+    ?>
+    <p><?=saudacao("bom dia", "Melissa")?></p>
+    <p><?=saudacao("bom dia", "Tanaka")?></p>
+    <p><?=saudacao("boa tarde")?></p>
 </body>
 </html>
