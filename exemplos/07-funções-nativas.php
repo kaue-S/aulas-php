bf528c9c86bd009809d271795d24400e133840d5
Kauê Evangelista <kauesilva38@hotmail.com>
2023-08-04 11:39:38 -0300

    Função implode()
    //função que transforma array em string

--- exemplos/07-funções-nativas.php
+++ exemplos/07-funções-nativas.php
@@ -52,10 +52,17 @@
     <p>maior valor: <?=max($valores)?></p>
     <p>Arredondamento: <?=round($valorQualquer)?></p>
     <hr>
-
-
     
     <h2>Arrais</h2>
+    <h3>implode()</h3>
+    <?php
+        //função que transforma array em string
+        $bandas = ["Rush", "Slayer", "Dio"];
+        $textoBandas = implode(" - ", $bandas);
+    ?>
+
+    <pre><?=var_dump($bandas)?></pre>
+    <pre><?=var_dump($textoBandas)?></pre>
 
     <h2>Filtros</h2>
 
