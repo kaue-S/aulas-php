c19d128e19e2f143db932376e4a6602f60f316e3
kaue-S <kauesilva38@hotmail.com>
2023-08-09 13:46:39 -0300

    Update exercicio-07.php

--- exercicios/exercicio-07.php
+++ exercicios/exercicio-07.php
@@ -22,13 +22,16 @@
         $nome = filter_var($_POST['nome'], FILTER_SANITIZE_SPECIAL_CHARS);
         $marca = filter_var($_POST['carros'], FILTER_SANITIZE_SPECIAL_CHARS);
         $preco = filter_var($_POST['preco'], FILTER_SANITIZE_NUMBER_FLOAT);
-        $disponivel = isset($_POST['disponivel']) ? $_POST['disponivel'] : "";
+
+        $disponivel = isset($_POST['disponivel']) ? $_POST['disponivel'] : "Disponibilidade não escolhida";
+
         $mensagem =filter_var($_POST['mensagem'], FILTER_SANITIZE_SPECIAL_CHARS);
 
-
-        if (empty($disponivel)) {?>
-            <p style="color:red">Por favor, selecione a disponibilidade.</p>
+        if ( empty($_POST["nome"]) || empty($_POST["preco"]) ) {?>
+            <p>Por favor, preencha todos os campos</p>
             <p><a href="exercicio-07.html">Voltar</a></p>
+
+       
         <?php
              }else{ 
         ?>
