360beafb1c2f79664bc8bd76abfcde377c7ae015
kaue-S <kauesilva38@hotmail.com>
2023-08-09 13:24:59 -0300

    Update exercicio-07.php

--- exercicios/exercicio-07.php
+++ exercicios/exercicio-07.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+
+    </style>
 </head>
 <body>
     <h1>Recebendo os dados</h1>
     <hr>
     <?php
-
-    //  $carros = [
-    //     "volvo",
-    //  "Porsche",
-    //     "Mercedes",
-    //     "Audi",
-    //  ];
 
     if ( empty($_POST["nome"]) || empty($_POST["preco"]) ) {?>
     <p>Por favor, preencha todos os campos</p>
@@ -26,7 +22,6 @@
         $nome = filter_var($_POST['nome'], FILTER_SANITIZE_SPECIAL_CHARS);
         $marca = filter_var($_POST['carros'], FILTER_SANITIZE_SPECIAL_CHARS);
         $preco = filter_var($_POST['preco'], FILTER_SANITIZE_NUMBER_FLOAT);
-        // $disponivel = filter_var($_POST['disponivel'], FILTER_SANITIZE_NUMBER_FLOAT);
         $disponivel = isset($_POST['disponivel']) ? $_POST['disponivel'] : "";
         $mensagem =filter_var($_POST['mensagem'], FILTER_SANITIZE_SPECIAL_CHARS);
 
